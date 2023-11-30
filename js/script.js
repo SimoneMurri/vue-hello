787829ec27654829320d22b4c42183420d93d29c
Simone Murri <simonemurri701@gmail.com>
2023-11-30 15:38:55 +0100

    bonus mettere in pagina un'immagine usando un data

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            message: 'Ciao a tutti!'
+            message: 'Ciao a tutti!',
+            image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQV6-DQF2pBwNFV9KzPafu9RghrNF1tZ8J3AA&usqp=CAU'
         }
     }
-}).mount('#app')
+}).mount('#app')    
